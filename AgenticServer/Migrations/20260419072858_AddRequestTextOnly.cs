12823c5be9ef139220f54dd3c2c40bc3c0997ff0
Artem Demianenko <you@example.com>
2026-04-19 09:54:46 +0200

    Fix: remove ignored files from index

---
+++ AgenticServer/Migrations/20260419072858_AddRequestTextOnly.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AgenticServer.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddRequestTextOnly : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
---
+++ AgenticServer/Migrations/20260419073747_AddLastReadAtToRoomMember.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AgenticServer.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddLastReadAtToRoomMember : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "LastReadAt",
+                table: "RoomMembers",
+                type: "datetime2",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "LastReadAt",
+                table: "RoomMembers");
+        }
+    }
+}
