fdadd47624597d58e4212fab28804f2d2471357d
Artem Demianenko <you@example.com>
2026-04-19 15:25:47 +0200

    Initial commit - AI Hackatone Chat Application

--- AgenticServer/Migrations/20260419121139_AddIsDeletedToMessages.Designer.cs
+++ AgenticServer/Migrations/20260419121139_AddIsDeletedToMessages.Designer.cs
@@ -4,6 +4,7 @@ using AgenticServer.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace AgenticServer.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260419121139_AddIsDeletedToMessages")]
+    partial class AddIsDeletedToMessages
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
